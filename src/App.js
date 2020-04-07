e2b37b47f09f01dc4d083b2d6a572f346ba79ed8
Thomas Shannon <thomasshannon1117@gmail.com>
2020-04-06 22:15:37 -0700

    - adjusting layout of card app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './Card';
 import cardData from './seed';
 import DeckNav from './DeckNav';
 
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  useEffect(
+    () => {
+      savedCallback.current = callback;
+    },
+    [callback]
+  );
+
+  useEffect(
+    () => {
+      const handler = (...args) => savedCallback.current(...args);
+
+      if (delay !== null) {
+        const id = setInterval(handler, delay);
+        return () => clearInterval(id);
+      }
+    },
+    [delay]
+  );
+};
+
 function App() {
   const [cardIndex, setIndex] = useState(0)
   const [deckIndex, setDeckIndex] = useState(0)
   const [currentDeck, setDeck] = useState(cardData[deckIndex])
   const [currentCard, setCard] = useState(currentDeck.cards[cardIndex])
+
   function toggleSide() {
     let item = {...currentCard}
     item.side = item.side === 'front' ? 'back' : 'front'
@@ -31,29 +55,38 @@ function App() {
     setCard(cardData[index].cards[0])
     setIndex(0)
   }
+  function cycleDeck() {
+    // useInterval(toggleSide, 1000)
+    // toggleSide()
+  }
   return (
     <div className="App">
-      <button className="Card-button Card-cycle-button"><span>Cycle deck</span> &#8634;</button>
-      <div className="Card-container">
-        <DeckNav active={currentDeck.title} decks={cardData} selectDeck={selectDeck} />
-        {/* <h2 className="Card-container-title">{currentDeck.title}</h2> */}
-        <div className="Card-actions-app">
-            <button className="Card-button Card-button-back" onClick={() => handleIndex(cardIndex - 1)}>&#x2190;</button>
-            <button className="Card-button Card-button-advance" onClick={() => handleIndex(cardIndex + 1)}>&#x2192;</button>
+      <button className="Card-button Card-cycle-button" onClick={cycleDeck}><span>Cycle deck</span> &#8634;</button>
+      <div className="Dash">
+        <div>
+          <DeckNav active={currentDeck.title} decks={cardData} selectDeck={selectDeck} />
         </div>
-        <Card
-          key={currentCard.id}
-          number={cardIndex + 1}
-          onClick={() => toggleSide()} 
-          advance={() => handleIndex(cardIndex + 1)}
-          goBack={() => handleIndex(cardIndex - 1)}
-          front={currentCard.front} 
-          back={currentCard.back} 
-          side={currentCard.side} 
-          language={currentCard.language} 
-        />
+        <div className="Card-container">
+          <div className="Card-container-inner">
+            <div className="Card-actions-app">
+                <button className="Card-button Card-button-back" onClick={() => handleIndex(cardIndex - 1)}>&#x2190;</button>
+                <button className="Card-button Card-button-advance" onClick={() => handleIndex(cardIndex + 1)}>&#x2192;</button>
+            </div>
+            <Card
+              key={currentCard.id}
+              number={cardIndex + 1}
+              onClick={() => toggleSide()} 
+              advance={() => handleIndex(cardIndex + 1)}
+              goBack={() => handleIndex(cardIndex - 1)}
+              front={currentCard.front} 
+              back={currentCard.back} 
+              side={currentCard.side} 
+              language={currentCard.language} 
+            />
+          </div>
+        </div>
+        </div>
       </div>
-    </div>
   );
 }
 
--- src/DeckNav.js
+++ src/DeckNav.js
@@ -3,11 +3,14 @@ import React from 'react';
 export default function DeckNav(props) {
     return (
         <nav className="Nav">
-            <ul>
-                {props.decks.map((deck, index) => {
-                    return <li key={index} className={props.active === deck.title ? 'active' : ''} onClick={() => props.selectDeck(index)}>{deck.title}</li>
-                })}
-            </ul>
+            <div className="Nav-children">
+                <h2 className="Nav-title">Decks</h2>
+                <ul>
+                    {props.decks.map((deck, index) => {
+                        return <li key={index} className={props.active === deck.title ? 'active' : ''} onClick={() => props.selectDeck(index)}>{deck.title}</li>
+                    })}
+                </ul>
+            </div>
         </nav>
     )
 }
