9266442f4d6e3e02bfb0cb4d1d56749b4a74c75d
Thomas Shannon <thomasshannon1117@gmail.com>
2020-04-06 20:45:05 -0700

    - updated seed

--- src/seed.js
+++ src/seed.js
@@ -7,24 +7,28 @@ let id = makeId()
 class JSCard {
     constructor(front, back) {
         this.id = id()
-        this.front = front
-        this.back = back
+        this.front = normalize(front)
+        this.back = normalize(back)
         this.language = 'js'
         this.side = 'front'
     }
 }
 
 function normalize(str) {
-    // let strArr = str.split('\n');
-    // strArr = strArr.filter(x => x.length !== 0)
-    // const smallestSpace = strArr.reduce((a, b) => {
-    //     console.log(a.search(/^\s*/), b)
-    //     return a
-    // }, '')
-    // console.log('s =>', smallestSpace)
-    // const trim = strArr.join('\n')
-    // console.log(strArr)
-    return str
+    let strArr = str.split('\n')
+    let smallest = Infinity
+    let filteredCollection = []
+    for (let i = 0; i < strArr.length; i++) {
+        if (strArr[i].length) {
+            let spaceCount = strArr[i].search(/\S|$/)
+            smallest = smallest > spaceCount ? spaceCount : smallest
+            filteredCollection.push(strArr[i])
+        }
+    }
+    for (let i = 0; i < filteredCollection.length; i++) {
+        filteredCollection[i] = filteredCollection[i].slice(smallest)
+    }
+    return filteredCollection.join('\n')
 }
 
 export default [
@@ -32,54 +36,51 @@ export default [
         title: 'Basics',
         cards: [
             new JSCard(
-`## What is the difference between var, let, and const?`,
-normalize(`
-// var declarations are globally scoped or function scoped
-// var variables can be updated and re-declared within its scope;
-var x = 1
-// let and const are block scoped
-// let variables can be updated but not re-declared
-let y = 2
-// const variables can neither be updated nor re-declared
-const name = 'mark'
-name = 'a' // would throw an ERROR
-    // indent test`)
-),
+            `## What is the difference between var, let, and const?`,
+            `
+            // var declarations are globally scoped or function scoped
+            // var variables can be updated and re-declared within its scope;
+            var x = 1
+            // let and const are block scoped
+            // let variables can be updated but not re-declared
+            let y = 2
+            // const variables can neither be updated nor re-declared
+            const name = 'mark'
+            name = 'a' // would throw an ERROR
+                // indent test
+            `
+            ),
             new JSCard(`## How are if/else/elseif statements written out?`,
-                `
-if (anyTrueCondition) {
-
-} else if (someOtherTrueCondition) {
-
-} else {
-    // do this thing if other conditions are not true
-}
-                `)
+            `
+                if (anyTrueCondition) {
+                    // execute this code...
+                } else if (someOtherTrueCondition) {
+                    // execute this code...
+                } else {
+                    // do this thing if other conditions are not true
+                }
+            `)
         ]
     },
     {
         title: 'Functions',
         cards: [
-            {
-                front: `## Write a function add which takes two inputs and adds them together`,
-                language: 'js',
-                back: `
-                function add(a, b) {
-                    return a + b
-                }
-                `,
-                side: 'front'
-            },
-            {
-                front: `## Write a function capitalize which takes a string and makes it uppercase`,
-                language: 'js',
-                back: `
-                function capitalize(str) {
-                    return str.toUpperCase()
-                }
-                `,
-                side: 'front'
-            }
+            new JSCard(
+                `## Write a function add which takes two inputs and adds them together`,
+                `
+                    function add(a, b) {
+                        return a + b
+                    }
+                `
+            ),
+            new JSCard(
+                `## Write a function capitalize which takes a string and makes it uppercase`,
+                `
+                    function capitalize(str) {
+                        return str.toUpperCase()
+                    }
+                `
+            )
         ]
     }
 ]
