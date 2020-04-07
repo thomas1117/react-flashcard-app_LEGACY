bcbaf529912b0f7755b432dd214e2925c86ecf73
Thomas Shannon <thomasshannon1117@gmail.com>
2020-04-07 16:59:11 -0700

    - refactoring into sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { themeProvider, ThemeProvider } from './ThemeContext';
-import './App.css';
 import Card from './Card';
 import Page from './Page';
 import cardData from './seed';
