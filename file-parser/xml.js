89035e330717631db86b5c89e876209f9b207f8c
Thomas Shannon <thomasshannon1117@gmail.com>
2020-04-17 01:04:06 -0700

    removed unused prototype method

--- file-parser/xml.js
+++ file-parser/xml.js
@@ -13,11 +13,6 @@ function decodeXML(m) {
   return decodeURIComponent(m)
 }
 
-String.prototype.replaceAll = function(search, replacement) {
-  var target = this;
-  return target.replace(new RegExp(search, 'g'), replacement);
-};
-
 function escapeInvalidXML(text) {
     const regex = /(?<=\<back\>)[\s\S]*?(?=\<\/back\>)/g
     const parsed = String(text).replace(regex, safelyEncodeXML)
@@ -26,27 +21,36 @@ function escapeInvalidXML(text) {
     return parsed2
 }
 
+function buildDeckFromXMLRoot(deckRoot) {
+  return deckRoot.map(d => {
+    const deckTitle = d.$.title
+    const language = d.$.language
+    const deck = {
+      title: deckTitle,
+      language: language,
+      // map over deck cards and convert values from array to string
+      cards: d.card.map(item => {
+          return {
+              front: decodeXML(item.front.join('')),
+              back: decodeXML(item.back.join('')),
+              meta: item.meta.join(''),
+              language: item.language || language
+          }
+      })
+    }
+    return deck
+  })
+}
+
+function storeToJSONSeed(decks) {
+  const deckToStore = JSON.stringify(decks, null, 4)
+  fs.writeFileSync(SEED_PATH, deckToStore)
+}
+
 fs.readFile(path.join(__dirname, './deck.xml'), 'utf8', (err, contents) => {
     const parsed = escapeInvalidXML(contents)
     parseXMLToString(parsed, function (err, result) {
-        // store the deck and grab root variables
-        const deckRoot = result.deck
-        const deckTitle = deckRoot.$.title
-        const language = deckRoot.$.language
-        const deck = {
-            title: deckTitle,
-            language: language,
-            // map over deck cards and convert values from array to string
-            cards: deckRoot.card.map(item => {
-                return {
-                    front: decodeXML(item.front.join('')),
-                    back: decodeXML(item.back.join('')),
-                    meta: item.meta.join(''),
-                    language: item.language || language
-                }
-            })
-        };
-        const deckToStore = JSON.stringify([deck], null, 4)
-        fs.writeFileSync(SEED_PATH, deckToStore)
+      const decks = buildDeckFromXMLRoot(result.decks.deck)
+      storeToJSONSeed(decks)
     })
 })
