f786bc928bc1f2983aafef6ec190b79f4c195b6b
Thomas Shannon <thomasshannon1117@gmail.com>
2020-04-24 02:51:28 -0700

    - added firebase start w/ db client

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import Topic from './pages/Topic';
 import { ThemeProvider } from '../ThemeContext';
 import driver from '../utils/driver';
+import db from '../utils/db-client';
 
 function App() {
+  useEffect(() => {
+    async function init() {
+      const x = await db.collection('topics').doc('rH13pSUxAIlACkOAU5pq').get()
+      console.log(x.data())
+    }
+    init()
+    
+    // db.collection('topics').add({title: 'js', decks: [
+    //   {
+    //     title: 'if statement',
+    //     cards: [
+    //       {
+    //         "front": "\n            ## if/else/else if statements\n            ",
+    //         "back": "\n                if (anyTrueCondition) {\n                    // execute this code...\n                } else if (someOtherTrueCondition) {\n                    // execute this code...\n                } else {\n                    // do this thing if other conditions are not true\n                }\n            ",
+    //         "meta": "if/else/else if statements",
+    //         "language": "js"
+    //       }
+    //     ]
+    //   }
+    // ]})
+  }, [])
   const settings = useSelector(state => state.settings)
   return (
     <ThemeProvider value={{theme: settings.activeTheme}}>
