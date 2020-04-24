9f9cfa8ffad764af4ba9500a1f1f62c3afc97823
Thomas Shannon <thomasshannon1117@gmail.com>
2020-04-24 00:17:56 -0700

    - added router start

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Deck from './pages/Deck';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   
   return (
-    <Deck />
+    <Router>
+      <Route path="/" component={Deck} />
+    </Router>
   );
 }
 export default App;
