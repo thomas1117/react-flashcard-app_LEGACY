b4a86717595cd99620c449a9de255676f1a351da
thomas1117 <thomasshannon117@yahoo.com>
2020-08-22 20:07:57 -0700

    update db connection with env variables

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,31 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('todo', 'root', '', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    storage: 'db.sqlite'
-})
+// console.log(process.env)
+function dbConfig() {
+    const o = {
+        host: 'localhost',
+        dialect: process.env.DIALECT,
+        storage: 'db.sqlite',
+    }
+    if (o.dialect === 'postgres') {
+        delete o.storage
+        o.host = 'localhost'
+    }
+    return o
+}
+let sequelize
+    if (process.env.NODE_ENV === 'production') {
+        sequelize = new Sequelize(process.env.DB_URL)
+    } else {
+        sequelize = new Sequelize(process.env.DB_NAME, process.env.USERNAME, process.env.PASSWORD, dbConfig())
+    }
+    
+(async () => {
+    
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+})()
 module.exports = sequelize
