1ce98b1afb3b76724816735934a6fbab4243f3b2
thomas1117 <thomasshannon117@yahoo.com>
2020-10-09 00:07:48 -0700

    leave comment around page load url on card

--- client/src/features/deck/Card.js
+++ client/src/features/deck/Card.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
 import CodeBlock from './CodeBlock'
 import { FaCopy } from 'react-icons/fa'
 import copy from 'copy-to-clipboard'
-import { Radio, RadioGroup, Stack, Button } from '@chakra-ui/core'
 
 import { useDeck } from './deckSlice'
 
 export default function Card(props) {
+  const history = useHistory()
+  const location = useLocation()
   const { activeCard, activeCardIndex, manageSide, activeSection } = useDeck()
   const { meta, front, back, side, language } = activeCard
 
@@ -15,6 +17,22 @@ export default function Card(props) {
     e.stopPropagation()
     copy(window.location)
   }
+  // TODO: these are pretty confusing... has to do with side effect of location query string
+  // TODO: There is a weird bug on load of back at the moment
+  useEffect(() => {
+    const pushState =
+      activeCard.side === 'back' && !location.search.includes('back')
+        ? '?back=true'
+        : null
+    history.push({ search: pushState })
+  }, [activeCard.side])
+
+  useEffect(() => {
+    if (location.search.includes('back')) {
+      history.push({ search: location.search })
+      manageSide()
+    }
+  }, [])
   return (
     <div className="Card" onClick={() => manageSide()}>
       <span className="Card-deck">
