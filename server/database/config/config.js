39632d02d2dc55bab6846bfffef609385129827a
thomas1117 <thomasshannon117@yahoo.com>
2020-11-08 20:32:29 -0800

    update db config

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
     "development": {
-        "username": "thomasshannon",
-        "password": "bitcoinsucks",
-        "database": "hex_deck",
+        "username": process.env.USERNAME,
+        "password": process.env.PASSWORD,
+        "database": process.env.DB_NAME,
         "host": "127.0.0.1",
         "dialect": "postgres"
     }
